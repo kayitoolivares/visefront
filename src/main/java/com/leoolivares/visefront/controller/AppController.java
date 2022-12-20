e3e8372af4bb76ef92c69194d52eeabcaa44e09a
EdgarHerm05 <102251731+LeoOlivares@users.noreply.github.com>
2022-12-20 15:20:06 -0600

    final

--- src/main/java/com/leoolivares/visefront/controller/AppController.java
+++ src/main/java/com/leoolivares/visefront/controller/AppController.java
@@ -2,11 +2,24 @@ package com.leoolivares.visefront.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AppController {
     @GetMapping("/")
     public String homePage(){
         return "index";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalle(@PathVariable("id") Integer id){
+
+        System.out.println(id);
+        return "detalle";
+    }
+    @GetMapping("/inve")
+    public String detalle(){
+
+        return "inventario";
     }
 }
