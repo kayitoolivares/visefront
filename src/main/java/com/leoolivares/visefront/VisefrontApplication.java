a6804e1f0a80a70aa75c7c3627b51b1aa6062277
EdgarHerm05 <102251731+LeoOlivares@users.noreply.github.com>
2022-12-19 19:49:45 -0600

    Initial commit

---
+++ src/main/java/com/leoolivares/visefront/VisefrontApplication.java
@@ -0,0 +1,13 @@
+package com.leoolivares.visefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisefrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoolivares/visefront/VisefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoolivares.visefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
